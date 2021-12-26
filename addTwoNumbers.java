5b420a283133dd654faeb4c569b7de9e8cce2b1a
Edildo <ruxtn7@163.com>
2021-12-26 15:08:42 +0800

    review linkedlist

--- addTwoNumbers.java
+++ addTwoNumbers.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 2. Add Two Numbers
@@ -11,6 +12,7 @@ import java.util.List;
 
 
 class addTwoNumbers {
+
     public static class ListNode {
         int val;
         ListNode next;
@@ -18,58 +20,112 @@ class addTwoNumbers {
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1.val == 0){
-            return l2;
+//        ListNode res = null; //为啥不能只初始化 一个 node：res后面会被覆盖，而我们需要返回的是头节点，所以需要初始化两个
+        ListNode head = null,res = null;
+
+//        if (l1.val == 0 || l1==null) return res=l2;
+//        if(l2.val == 0 || l2==null) return res=l1;
+
+        while(l1 !=null || l2 !=null){
+            //需要先对head判空，如果head为null需要赋值，如果head不为null就不动，最终需要返回的是head
+            //机智的做法就是直接new int，放两个node的val，下面这种办法还没有计算进位carry
+            if(head == null) head = res = new ListNode(l1.val+l2.val);
+            else res = res.next = new ListNode(l1.val+l2.val);
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        if(l2.val == 0){
-            return l1;
-        }
-        // System.out.println("wait");
-        if(l1.val + l2.val >=10){
-            ListNode l3 = new ListNode(l1.val + l2.val-10);
-            // l3.next.val = 1;
-            return l3;
-        }
-        else{
-            ListNode l3 = new ListNode(l1.val + l2.val);
-            return l3;
-        }
+
+        return head;
+
     }
 
      //copy from official ans
-     public ListNode addTwoNumbers_official(ListNode l1, ListNode l2) {
-         ListNode head = null, tail = null;
-         int carry = 0;
-         while (l1 != null || l2 != null){
+    public ListNode addTwoNumbers_official(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null){
 
-             int n1 = l1 !=null ? l1.val : 0;
-             int n2 = l2 !=null ? l2.val : 0;
-             int sum = n1 + n2 + carry;
-             carry = (n1+n2+carry)/10;
-             if(head == null){
-                 head = tail = new ListNode(sum%10);
-             }else{
-                 tail.next = new ListNode(sum%10);
-                 tail=tail.next;
-             }
-             if(l1!=null){
-                 l1=l1.next;
-             }
-             if(l2!=null){
-                 l2=l2.next;
-             }
-         }
-         if(carry>0){
-             tail.next=new ListNode(carry);
-             tail=tail.next;
-         }
-         return head;
-     }
+            int n1 = l1 !=null ? l1.val : 0;
+            int n2 = l2 !=null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            carry = (n1+n2+carry)/10;
+            if(head == null){
+                head = tail = new ListNode(sum%10);
+            }else{
+                tail.next = new ListNode(sum%10);
+                tail=tail.next;
+            }
+            if(l1!=null){
+                l1=l1.next;
+            }
+            if(l2!=null){
+                l2=l2.next;
+            }
+        }
+        if(carry>0){
+            tail.next=new ListNode(carry);
+            tail=tail.next;
+        }
+        return head;
+    }
 
-    public static void main(String args[]){
-        String s = "ccc";
-        String sub = addTwoNumbers(s);
-        System.out.println(sub);
+    public static class MainClass {
+        public static int[] stringToIntegerArray(String input) {
+            input = input.trim();
+            input = input.substring(1, input.length() - 1);
+            if (input.length() == 0) {
+                return new int[0];
+            }
+
+            String[] parts = input.split(",");
+            int[] output = new int[parts.length];
+            for(int index = 0; index < parts.length; index++) {
+                String part = parts[index].trim();
+                output[index] = Integer.parseInt(part);
+            }
+            return output;
+        }
+
+        public static ListNode stringToListNode(String input) {
+            // Generate array from the input
+            int[] nodeValues = stringToIntegerArray(input);
+
+            // Now convert that list into linked list
+            ListNode dummyRoot = new ListNode(0);
+            ListNode ptr = dummyRoot;
+            for(int item : nodeValues) {
+                ptr.next = new ListNode(item);
+                ptr = ptr.next;
+            }
+            return dummyRoot.next;
+        }
+
+        public static String listNodeToString(ListNode node) {
+            if (node == null) {
+                return "[]";
+            }
+
+            String result = "";
+            while (node != null) {
+                result += Integer.toString(node.val) + ", ";
+                node = node.next;
+            }
+            return "[" + result.substring(0, result.length() - 2) + "]";
+        }
+
+        public static void main(String[] args) {
+            String line1="[1,2,3]";
+            String line2="[4,5,6]";
+            ListNode l1 = stringToListNode(line1);
+            ListNode l2 = stringToListNode(line2);
+
+            ListNode ret = new addTwoNumbers().addTwoNumbers(l1, l2);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
     }
 }
