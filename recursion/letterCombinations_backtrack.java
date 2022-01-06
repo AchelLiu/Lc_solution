e72b16b8a694235d81a41464e64f617aa06d3883
liuzhining3 <liuzhining3@jd.com>
2022-01-06 11:28:56 +0800

    letter recursion update

--- recursion/letterCombinations_backtrack.java
+++ recursion/letterCombinations_backtrack.java
@@ -15,10 +15,13 @@ import java.util.Map;
  */
 
 
-
-//official solution
+/**
+ * official solution
+ * 这里的回溯思想跟stack排列组合思想类似，最核心的部分是在循环块里面：先入栈再递归，递归语句之后有一个出栈pop/删最后字符deleteCharAt的操作.
+ * 当递归满足return条件（index==length）之后，由于出栈操作，index会回溯之前缓存的步骤（缓存了当时的index和i），重新进入递归和循环
+ */
 class letterCombinations_backtrack {
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
         List<String> combinations = new ArrayList<String>();
         if (digits.length() == 0) {
             return combinations;
@@ -37,7 +40,7 @@ class letterCombinations_backtrack {
         return combinations;
     }
 
-    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+    public static void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
         if (index == digits.length()) {
             combinations.add(combination.toString());
         } else {
@@ -50,5 +53,9 @@ class letterCombinations_backtrack {
                 combination.deleteCharAt(index);
             }
         }
+    }
+    public static void main(String args[]){
+        String s = "23";
+        System.out.println(letterCombinations(s));
     }
 }
